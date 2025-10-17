1ff5e1e9dfa0935e762bf55b6fee6c1170940b79
heinrichschiller <heinrich@heinrich-schiller.de>
2025-10-17 11:39:40 +0200

    fixed DOMXPath class name

--- src/XmlSigner.php
+++ src/XmlSigner.php
@@ -180,7 +180,7 @@ final class XmlSigner
 
         $signatureValue = $this->cryptoSigner->computeSignature($c14nSignedInfo);
 
-        $xpath = new DOMXpath($xml);
+        $xpath = new DOMXPath($xml);
         $signatureValueElement = $this->xmlReader->queryDomNode($xpath, '//SignatureValue', $signatureElement);
         $signatureValueElement->nodeValue = base64_encode($signatureValue);
     }
